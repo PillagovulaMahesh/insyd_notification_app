482e2faf9696c9c868dc8d2d7f94a657106e54f9
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-08-21 21:10:43 +0530

    Update db.js

---
+++ insyd_notification_app/backend/src/config/db.js
@@ -0,0 +1,19 @@
+// backend/src/config/db.js
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    // Use environment variable for MongoDB connection string
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
