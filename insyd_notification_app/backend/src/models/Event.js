6a02ff28432dd56dd82c23de49adb619afc35573
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-08-21 21:13:04 +0530

    Update Event.js

---
+++ insyd_notification_app/backend/src/models/Event.js
@@ -0,0 +1,17 @@
+// backend/src/models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
